d34072a1e34c45da36c77e38442671b4aba8e00e
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-28 17:34:10 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -60,7 +60,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "strapi" {
   ami           = data.aws_ami.ubuntu.id  # Correct AMI ID for ap-south-1
-  instance_type = "t2.medium"              # Changed to t2.medium
+  instance_type = var.instance_type             # Changed to t2.medium
   key_name      = "devops"                  # Your key pair name
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
